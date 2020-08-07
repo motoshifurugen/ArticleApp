87e0ff70012381cadb85a0b57831d67b212a39be
motoshifurugen <amanogawa_m@icloud.com>
2020-08-08 08:24:49 +0900

    認証機能追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
@@ -17,12 +19,18 @@ class ArticleController extends Controller
 
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $article = new Article();
+        $article->user_id = 1;
+        $article->title = request('title');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles');
     }
 
     public function show(Article $article)
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,4 +1,29 @@
-@foreach($articles as $article)
-    {{$article->title}}
-    {{$article->body}}
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+  <br/>
+  <button type="button" onclick="location.href='/articles/create'">記事を作る</button>
+  <br/>
+
+  <table border="1" cellspacing="0" cellpadding="5">
+    <tr align="center">
+      <th>タイトル</th>
+      <th>内容</th>
+      <th colspan="2"></th>
+    </tr>
+    @foreach($articles as $article)
+      <tr>
+        <td align="center">{{ $article->title }}</td>
+        <td align="center">{{ $article->body }}</td>
+        <td><button type="button" onclick="location.href='articles/{{ $article->id }}/edit'">編集</button></td>
+        <td valign="middle">
+            <form action="/articles/{{ $article->id }}" method="POST" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit">削除</button>
+            </form>
+        </td>
+      </tr>
+    @endforeach
+  </table>
+@endsection
